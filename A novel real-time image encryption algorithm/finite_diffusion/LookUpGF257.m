5d2b2c96c2d02dcb656af60f9768e0324f1e89b1
WangQuanyv <quanyvwang@cumt.edu.cn>
2024-07-17 18:17:40 +0800

    Delete A novel real-time image encryption algorithm directory

--- A novel real-time image encryption algorithm/finite_diffusion/LookUpGF257.m
+++
@@ -1,5 +0,0 @@
-% 基于GF(257)域的乘法运算函数
-% 两个输入变量
-function y = LookUpGF257(x0,x1,TBL)
-     y = TBL(x0+2,x1+2)-1;
-end
--- A novel real-time image encryption algorithm/finite_diffusion/LookUpGF2p8Ex.m
+++
@@ -1,5 +0,0 @@
-% 基于GF(2^8)域的减法运算函数
-function y = LookUpGF2p8Ex(x0,x1,TBL)
-      t = x1+1;
-      y = find(TBL(t,:)==x0)-1;
-end
